208d8bc537cbd6f3b67dec650c4b5fde7c2037c3
Cintulova <eliska.cintulova@capgemini.com>
2023-03-14 13:37:22 +0100

    variables to Customer model added

--- src/main/java/com/cintulova/TaskInsuranceApp/model/Customer.java
+++ src/main/java/com/cintulova/TaskInsuranceApp/model/Customer.java
@@ -1,4 +1,40 @@
 package com.cintulova.TaskInsuranceApp.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "tbl_customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "customer id")
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "middle_name")
+    private String MiddleName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+
 }
