66ba584bd6df338bbbbf701f4e684c2f55f1c431
Cintulova <eliska.cintulova@capgemini.com>
2023-03-14 13:13:00 +0100

    first commit

---
+++ src/main/java/com/cintulova/TaskInsuranceApp/TaskInsuranceAppApplication.java
@@ -0,0 +1,13 @@
+package com.cintulova.TaskInsuranceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskInsuranceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskInsuranceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cintulova/TaskInsuranceApp/TaskInsuranceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cintulova.TaskInsuranceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskInsuranceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
