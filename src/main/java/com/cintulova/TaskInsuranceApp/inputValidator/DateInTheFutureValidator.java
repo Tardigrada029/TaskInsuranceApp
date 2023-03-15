0bb10f70cc7c953f3984aae50d0dea4fcb503eaa
Cintulova <eliska.cintulova@capgemini.com>
2023-03-15 08:31:41 +0100

    DateInThePastValidator and DateInTheFutureValidator implemented

--- src/main/java/com/cintulova/TaskInsuranceApp/inputValidator/DateInTheFutureValidator.java
+++ src/main/java/com/cintulova/TaskInsuranceApp/inputValidator/DateInTheFutureValidator.java
@@ -2,9 +2,14 @@ package com.cintulova.TaskInsuranceApp.inputValidator;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class DateInTheFutureValidator {
 
-
+    public Boolean isValidDateInTheFuture(LocalDate date) {
+        LocalDate today = LocalDate.now();
+        return date.isAfter(today);
+    }
 
 }
--- src/main/java/com/cintulova/TaskInsuranceApp/inputValidator/DateInThePastValidator.java
+++ src/main/java/com/cintulova/TaskInsuranceApp/inputValidator/DateInThePastValidator.java
@@ -2,9 +2,14 @@ package com.cintulova.TaskInsuranceApp.inputValidator;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class DateInThePastValidator {
 
-
+    public Boolean isValidDateInThePast(LocalDate date) {
+        LocalDate today = LocalDate.now();
+        return date.isBefore(today);
+    }
 
 }
