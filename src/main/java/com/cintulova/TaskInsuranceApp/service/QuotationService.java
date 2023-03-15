19fc543ae6cfc60559413ca2f09ab95a64280d6a
Cintulova <eliska.cintulova@capgemini.com>
2023-03-15 10:33:17 +0100

    save, get and delete implemented in Quotation service

--- src/main/java/com/cintulova/TaskInsuranceApp/service/QuotationService.java
+++ src/main/java/com/cintulova/TaskInsuranceApp/service/QuotationService.java
@@ -1,7 +1,29 @@
 package com.cintulova.TaskInsuranceApp.service;
 
+import com.cintulova.TaskInsuranceApp.model.Quotation;
+import com.cintulova.TaskInsuranceApp.repository.QuotationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class QuotationService {
+
+    private final QuotationRepository quotationRepository;
+
+    public QuotationService(QuotationRepository quotationRepository) {
+        this.quotationRepository = quotationRepository;
+    }
+
+    public void saveQuotation(Quotation quotation) {
+        quotationRepository.save(quotation);
+    }
+
+    public Optional<Quotation> getQuotationById(Long id) {
+        return quotationRepository.findById(id);
+    }
+
+    public void deleteQuotationById(Long id) {
+        quotationRepository.deleteById(id);
+    }
 }
