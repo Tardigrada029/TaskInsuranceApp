2026375fb453eb9da826e8215c65c0c1da13cdc3
Cintulova <eliska.cintulova@capgemini.com>
2023-03-15 10:29:04 +0100

    save, get and delete implemented in Customer service

--- src/main/java/com/cintulova/TaskInsuranceApp/service/CustomerService.java
+++ src/main/java/com/cintulova/TaskInsuranceApp/service/CustomerService.java
@@ -1,7 +1,30 @@
 package com.cintulova.TaskInsuranceApp.service;
 
+import com.cintulova.TaskInsuranceApp.model.Customer;
+import com.cintulova.TaskInsuranceApp.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class CustomerService {
+public class CustomerService  {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public void saveCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public Optional<Customer> getUserById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public void deleteCustomerById(Long id) {
+        customerRepository.deleteById(id);
+    }
+
 }
