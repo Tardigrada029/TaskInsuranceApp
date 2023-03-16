6470fe78d5fb15fb60d1e76753a019b54f2c7939
Cintulova <eliska.cintulova@capgemini.com>
2023-03-16 13:28:25 +0100

    CustomerController implemented

---
+++ src/main/java/com/cintulova/TaskInsuranceApp/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package com.cintulova.TaskInsuranceApp.controller;
+
+import com.cintulova.TaskInsuranceApp.model.Customer;
+import com.cintulova.TaskInsuranceApp.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Customer> getCustomerById(@PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PostMapping
+    public Customer saveCustomer(@RequestBody Customer customer) {
+        return customerService.saveCustomer(customer);
+    }
+
+    @PatchMapping("/{id}")
+    public Customer updateCustomerById(@PathVariable Long id, @RequestBody Customer customer) {
+        return customerService.updateCustomerById(customer, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomerById(@PathVariable Long id) {
+        customerService.deleteCustomerById(id);
+    }
+
+}
