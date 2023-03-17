aee65f3695cbe59901c9e115449ff7e8a811a0a1
Cintulova <eliska.cintulova@capgemini.com>
2023-03-17 09:55:28 +0100

    PathVariable annotation added to SubscriptionService

--- src/main/java/com/cintulova/TaskInsuranceApp/service/SubscriptionService.java
+++ src/main/java/com/cintulova/TaskInsuranceApp/service/SubscriptionService.java
@@ -3,6 +3,7 @@ package com.cintulova.TaskInsuranceApp.service;
 import com.cintulova.TaskInsuranceApp.model.Subscription;
 import com.cintulova.TaskInsuranceApp.repository.SubscriptionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -23,7 +24,7 @@ public class SubscriptionService {
         return subscriptionRepository.save(subscription);
     }
 
-    public Optional<Subscription> getSubscriptionById(Long id) {
+    public Optional<Subscription> getSubscriptionById(@PathVariable Long id) {
         if (subscriptionRepository.findById(id).isEmpty()) {
             throw new NoSuchElementException("Could not find subscription with id " + id + ".");
         }
